c87479287e464207e920a5aa1446de66eb7de7c8
Starkthegreat <EmmanuelKipropKimutai@gmail.com>
2024-10-23 20:17:50 +0300

    update: update linking of lines

--- roadmap/script.js
+++ roadmap/script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         canvases.push(canvas);
 
         if (milestones.length > 1) {
-            drawLine(canvases[canvases.length - 2], canvases[canvases.length - 1], milestones[milestones.length - 2], milestone);
+            drawLines();
         }
 
         milestone.addEventListener('mousedown', dragStart);
@@ -45,37 +45,60 @@ document.addEventListener('DOMContentLoaded', () => {
         currentMilestone = null;
     }
 
-    function drawLine(canvas1, canvas2, milestone1, milestone2) {
-        const ctx1 = canvas1.getContext('2d');
-        const ctx2 = canvas2.getContext('2d');
+    // function drawLine(canvas1, canvas2, milestone1, milestone2) {
+    //     const ctx1 = canvas1.getContext('2d');
+    //     const ctx2 = canvas2.getContext('2d');
 
-        const rect1 = milestone1.getBoundingClientRect();
-        const rect2 = milestone2.getBoundingClientRect();
+    //     const rect1 = milestone1.getBoundingClientRect();
+    //     const rect2 = milestone2.getBoundingClientRect();
 
-        ctx1.beginPath();
-        ctx1.moveTo(rect1.x + rect1.width / 2, rect1.y + rect1.height / 2);
-        ctx1.lineTo(rect2.x + rect2.width / 2, rect2.y + rect2.height / 2);
-        ctx1.stroke();
+    //     ctx1.beginPath();
+    //     ctx1.moveTo(rect1.x + rect1.width / 2, rect1.y + rect1.height / 2);
+    //     ctx1.lineTo(rect2.x + rect2.width / 2, rect2.y + rect2.height / 2);
+    //     ctx1.stroke();
 
-        ctx2.beginPath();
-        ctx2.moveTo(rect1.x + rect1.width / 2, rect1.y + rect1.height / 2);
-        ctx2.lineTo(rect2.x + rect2.width / 2, rect2.y + rect2.height / 2);
-        ctx2.stroke();
+    //     ctx2.beginPath();
+    //     ctx2.moveTo(rect1.x + rect1.width / 2, rect1.y + rect1.height / 2);
+    //     ctx2.lineTo(rect2.x + rect2.width / 2, rect2.y + rect2.height / 2);
+    //     ctx2.stroke();
+    // }
+
+    function drawLines() {
+        canvases.forEach((canvas, index) => {
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Draw lines between consecutive milestones
+            for (let i = 0; i < milestones.length - 1; i++) {
+                const currMilestone = milestones[i];
+                const nextMilestone = milestones[i + 1];
+
+                const rect1 = currMilestone.getBoundingClientRect();
+                const rect2 = nextMilestone.getBoundingClientRect();
+
+                ctx.beginPath();
+                ctx.moveTo(rect1.x + rect1.width / 2, rect1.y + rect1.height / 2);
+                ctx.lineTo(rect2.x + rect2.width / 2, rect2.y + rect2.height / 2);
+                ctx.stroke();
+            }
+        });
     }
+
 
     document.addEventListener('mousemove', (e) => {
         if (isDragging && currentMilestone) {
             currentMilestone.style.left = `${e.clientX - currentMilestone.offsetWidth / 2}px`;
             currentMilestone.style.top = `${e.clientY - currentMilestone.offsetHeight / 2}px`;
 
-            canvases.forEach((canvas, index) => {
-                const ctx = canvas.getContext('2d');
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawLines();
+            // canvases.forEach((canvas, index) => {
+            //     const ctx = canvas.getContext('2d');
+            //     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-                if (index > 0) {
-                    drawLine(canvases[index - 1], canvas, milestones[index - 1], milestones[index]);
-                }
-            });
+            // if (index > 0) {
+            // drawLine(canvases[index - 1], canvas, milestones[index - 1], milestones[index]);
+            // }
+            // });
         }
     });
 
