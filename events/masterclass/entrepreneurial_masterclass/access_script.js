b616464db8d6f548014ce7d59a37dcb8f900ced4
Starkthegreat <EmmanuelKipropKimutai@gmail.com>
2025-03-25 12:38:16 +0300

    chore: update privacy policy

---
+++ events/masterclass/entrepreneurial_masterclass/access_script.js
@@ -0,0 +1,7 @@
+// Check if the email used has paid for the masterclass.
+// Make an API call to intersend to confirm payment status.
+// If paid, give access else, take the person back to the intersend payment page.
+// Three step payment flow, Pay=> Make Payment, Fill in details => Email and Phone, Receive access to masterclass.
+document.addEventListener('DOMContentLoaded', () => {
+
+})
